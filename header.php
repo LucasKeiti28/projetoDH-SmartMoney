ca7f67ce4a6c86f83863da0a764b2e2211ab2e80
rserafim <rafael_reale@yahoo.com.br>
2019-04-10 21:25:46 -0300

    adicionado modal do bt abrir minha conta

--- header.php
+++ header.php
@@ -1,34 +1,75 @@
 <nav class="navbar navbar-static-top navbar-default">
-  <div class="container">
-    <div class="navbar-header navbar-left">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="#">
-        <img alt="Brand" src="img/logo.png">
-      </a>
+    <div class="container">
+        <div class="navbar-header navbar-left">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">
+                <img alt="Brand" src="img/logo.png">
+            </a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <!-- <li class="active"><a href="#index.php">Home<span class="sr-only">(current)</span></a></li> -->
+                <li><a href="index.php">Home</a></li>
+                <li><a href="sobre.php">Sobre nós</a></li>
+                <li><a href="#">Cartão de crédito</a></li>
+                <li><a href="contato.php">Contatos</a></li>
+            </ul>
+
+            <ul class="nav navbar-nav navbar-right">
+                <li><a class="login bold" href="index-posts.php">Login</a></li>
+                <li><a href="#" class="cadastre-se" role="button" data-toggle="modal" data-target="#novaConta">Abrir
+                        minha conta</a></li>
+            </ul>
+
+
+        </div><!-- /.navbar-collapse -->
+
     </div>
-
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav">
-        <!-- <li class="active"><a href="#index.php">Home<span class="sr-only">(current)</span></a></li> -->
-        <li><a href="index.php">Home</a></li>
-        <li><a href="sobre.php">Sobre nós</a></li>
-        <li><a href="#">Cartão de crédito</a></li>
-        <li><a href="contato.php">Contatos</a></li>
-      </ul>
-
-      <ul class="nav navbar-nav navbar-right">
-        <li><a class="login bold" href="index-posts.php">Login</a></li>
-        <li><a href="#" class="cadastre-se" role="button">Abrir minha conta</a></li>
-      </ul>
-
-
-    </div><!-- /.navbar-collapse -->
-
-  </div>
 </nav>
+<div class="modal fade" id="novaConta" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true"
+                        class="bt-fechar-modal">&times;</span></button>
+            </div>
+            <div class="modal-body">
+                <div class="padding-30">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <h3>Insira os dados para cadastrar</h3>
+                        </div>
+                        <div class="col-md-12">
+                            <form class="contato">
+                                <div class="form-group">
+                                    <input id="nova-conta-nome" type="text" placeholder="Nome Completo"
+                                        class="form-control">
+                                    <input id="nova-conta-cpf" type="number" placeholder="CPF" class="form-control">
+                                    <input id="nova-conta-email" type="text" placeholder="E-mail" class="form-control">
+                                    <input id="nova-conta-email-confirma" type="text" placeholder="Confirmar e-mail"
+                                        class="form-control">
+                                    <div class="mt-15">
+                                        <input type="checkbox" id="termo" name="termo"><label for="termo">Li e concordo
+                                            com a</label> <a href="#" class="azul">política de privacidade</a>
+                                    </div>
+                                    <div class="text-right">
+                                        <button type="submit" class="btn btn-secundario btn-block">EFETUAR
+                                            CADASTRO</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- index.php
+++ index.php
@@ -3,20 +3,20 @@
   include_once "header.php" ;
 ?>
 <main class="display-flex-center">
-  <div class="container">
-    <div class="row display-flex-center">
-      <div class=" col-md-7">
-        <div class="texto-principal-mobile">
-          <h1 class="bold">Lorem Ipsum</h1>
-          <h2>Is simple dummy text of the printing and typesetting industry.</h2>
-          <a class="link-principal azul">Lorem ipsum dolor sit amet</a>
+    <div class="container">
+        <div class="row display-flex-center">
+            <div class=" col-md-7">
+                <div class="texto-principal-mobile">
+                    <h1 class="bold">Lorem Ipsum</h1>
+                    <h2>Is simple dummy text of the printing and typesetting industry.</h2>
+                    <a class="link-principal azul">Lorem ipsum dolor sit amet</a>
+                </div>
+            </div>
+            <div class="col-md-5">
+                <img class="img-principal" src="img/celular-home.png" alt="">
+            </div>
         </div>
-      </div>
-      <div class=" col-md-5">
-        <img class="img-principal" src="img/celular-home.png" alt="">
-      </div>
     </div>
-  </div>
 </main>
 
 <?php
